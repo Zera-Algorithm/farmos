9513af857069f9b3bf276583412dc6e370f31df6
Shihesy <2110459069@qq.com>
2023-07-10 21:54:43 +0800

    暂时去掉颜色

--- kern/lib/printf.c
+++ kern/lib/printf.c
@@ -68,8 +68,8 @@ void _log(const char *file, int line, const char *func, const char *fmt, ...) {
 
 	mtx_lock(&pr_lock);
 	// 输出日志头
-	printfNoLock("%s %2d %12s:%-4d %12s()" SGR_RESET ": ",
-		     FARM_INFO "[INFO]" SGR_RESET SGR_BLUE, cpu_this_id(), file, line, func);
+	printfNoLock("%s %2d %12s:%-4d %12s()"  ": ",
+		      "[INFO]", cpu_this_id(), file, line, func);
 	// 输出实际内容
 	vprintfmt(output, NULL, fmt, ap);
 	mtx_unlock(&pr_lock);
