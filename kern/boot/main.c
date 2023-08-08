5cbd6971902f369c2634cdf0aeb142dffd8faddd
Shihesy <2110459069@qq.com>
2023-08-09 00:20:57 +0800

    logo

--- kern/boot/main.c
+++ kern/boot/main.c
@@ -121,6 +121,19 @@ static inline void hart_init() {
 	plicInitHart(); // 启动中断控制器，开始接收中断
 }
 
+static inline void logo() {
+	printf("\n");
+	printf("8888888888     d8888 8888888b.  888b     d888  .d88888b.   .d8888b.\n");
+	printf("888           d88888 888   Y88b 8888b   d8888 d88P\" \"Y88b d88P  Y88b\n");
+	printf("888          d88P888 888    888 88888b.d88888 888     888 Y88b.\n");
+	printf("8888888     d88P 888 888   d88P 888Y88888P888 888     888  \"Y888b.\n");
+	printf("888        d88P  888 8888888P\"  888 Y888P 888 888     888     \"Y88b.\n");
+	printf("888       d88P   888 888 T88b   888  Y8P  888 888     888       \"888\n");
+	printf("888      d8888888888 888  T88b  888   \"   888 Y88b. .d88P Y88b  d88P\n");
+	printf("888     d88P     888 888   T88b 888       888  \"Y88888P\"   \"Y8888P\"\n");
+	printf("\n");
+}
+
 // start() jumps here in supervisor mode on all CPUs.
 void main() {
 	if (hart_first == 1) {
@@ -129,15 +142,16 @@ void main() {
 
 		// 初始化串口
 		cons_init();
+		logo();
 		printf("FarmOS kernel is booting (on hart %d) total: %d\n", cpu_this_id(), NCPU);
 
-		// // 测试时间
-		// for (int i = 0; i <= 500; i++) {
-		// 	for (int j = 0; j <= 100000000; j++) {
-		// 		asm volatile("nop");
-		// 	}
-		// 	printf("now realtime: %ld\n", getRealTime());
-		// }
+		#ifdef VIRT
+		printf("FarmOS is run on virt machine!\n");
+		#elif QEMU_SIFIVE
+		printf("FarmOS is run on qemu sifive_u!\n");
+		#else
+		printf("FarmOS is run on real hifive unmatched board!\n");
+		#endif
 
 		// 读取 dtb
 		// #ifdef QEMU_SIFIVE
